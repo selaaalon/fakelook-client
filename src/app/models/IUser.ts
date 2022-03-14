cf97f5f924f7da5db6a8993272152dec750a2cb5
dganit <dganit@example.com>
2022-03-14 14:06:20 +0200

    Connecting to DB

---
+++ src/app/models/IUser.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    id? : number,
+    email : string,
+    name : string,
+    userName : string,
+    password : string,
+    birthDate : Date,
+    address : string,
+    job : string
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private usersUrl = "https://localhost:44349/Users";
 
-  
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get<any>(this.usersUrl);
+  }
 }
